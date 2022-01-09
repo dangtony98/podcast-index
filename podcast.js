0939feee61e323be18b4e771c98a6884655f4cf0
Tuan Dang <tuandang@dhcp-vl2051-403.eduroam.cornell.edu>
2022-01-09 14:35:24 -0500

    Save example use-cases of podcast index queries

--- podcast.js
+++ podcast.js
@@ -12,7 +12,7 @@ var hash4Header = sha1Hash.digest('hex');
 
 // arbitrary search
 // (async () => {
-//     const SEARCH_TERM = 'daily';
+//     const SEARCH_TERM = 'historypod';
 //     let data;
 //     try {
 //         data = await axios.get(
@@ -34,26 +34,126 @@ var hash4Header = sha1Hash.digest('hex');
 //     console.log(data.data.feeds);
 // })();
 
-// search for specific podcast matching ID
-(async () => {
-    const ID = 743229; // The Daily
-    let data;
-    try {
-        data = await axios.get(
-            `https://api.podcastindex.org/api/1.0/episodes/byfeedid?id=${ID}&pretty`,
-            {
-              headers: {
-                "User-Agent": `Auledge/1.8.7`,
-                "X-Auth-Key": process.env.API_KEY,
-                "X-Auth-Date": apiHeaderTime,
-                "Authorization": hash4Header
-              },
-            }
-          );
-    } catch (err) {
-        console.log(err);
-    }
+// get list of podcast categories
+// (async () => {
+//     // const SEARCH_TERM = 'historypod';
+//     let data;
+//     try {
+//         data = await axios.get(
+//             `https://api.podcastindex.org/api/1.0/categories/list?pretty`,
+//             {
+//               headers: {
+//                 "User-Agent": `Auledge/1.8.7`,
+//                 "X-Auth-Key": process.env.API_KEY,
+//                 "X-Auth-Date": apiHeaderTime,
+//                 "Authorization": hash4Header
+//               },
+//             }
+//           );
+//     } catch (err) {
+//         console.log(err);
+//     }
     
-    // console.log('data');
-    // console.log(data.data.items);
-})();
+//     console.log('data');
+//     console.log(data.data.feeds);
+//     // console.log(data.data.feeds);
+// })();
+
+// search for specific podcast episodes matching ID
+// (async () => {
+//     const ID = 743229; // The Daily
+//     const ID2 = 414307; // Martiak Update 1
+//     const ID3 = 4258139; // Martiak Update 2
+//     let data;
+//     try {
+//         data = await axios.get(
+//             `https://api.podcastindex.org/api/1.0/episodes/byfeedid?id=${ID2}&pretty`,
+//             {
+//               headers: {
+//                 "User-Agent": `Auledge/1.8.7`,
+//                 "X-Auth-Key": process.env.API_KEY,
+//                 "X-Auth-Date": apiHeaderTime,
+//                 "Authorization": hash4Header
+//               },
+//             }
+//           );
+//     } catch (err) {
+//         console.log(err);
+//     }
+    
+//     console.log('data');
+//     console.log(data.data.items.length);
+//     console.log(data.data.items);
+// })();
+
+// search for specific podcast episodes matching feed URL
+// (async () => {
+//     const URL = 'https://martiakmarketupdate.libsyn.com/rss';
+//     let data;
+//     try {
+//         data = await axios.get(
+//             `https://api.podcastindex.org/api/1.0/episodes/byfeedurl?url=${URL}&pretty`,
+//             {
+//               headers: {
+//                 "User-Agent": `Auledge/1.8.7`,
+//                 "X-Auth-Key": process.env.API_KEY,
+//                 "X-Auth-Date": apiHeaderTime,
+//                 "Authorization": hash4Header
+//               },
+//             }
+//           );
+//     } catch (err) {
+//         console.log(err);
+//     }
+    
+//     console.log('data');
+//     console.log(data);
+//     console.log(data.data.items)
+// })();
+
+// search for specific podcast matching ID
+// (async () => {
+//     const ID = 743229; // The Daily
+//     let data;
+//     try {
+//         data = await axios.get(
+//             `https://api.podcastindex.org/api/1.0/podcasts/byfeedid?id=${ID}&pretty`,
+//             {
+//               headers: {
+//                 "User-Agent": `Auledge/1.8.7`,
+//                 "X-Auth-Key": process.env.API_KEY,
+//                 "X-Auth-Date": apiHeaderTime,
+//                 "Authorization": hash4Header
+//               },
+//             }
+//           );
+//     } catch (err) {
+//         console.log(err);
+//     }
+    
+//     console.log('data');
+//     console.log(data); // check by feed?.id if exists
+// })();
+
+// get recent feeds
+// (async () => {
+//     let data;
+//     try {
+//         data = await axios.get(
+//             `https://api.podcastindex.org/api/1.0/recent/episodes?max=7&pretty`,
+//             {
+//               headers: {
+//                 "User-Agent": `Auledge/1.8.7`,
+//                 "X-Auth-Key": process.env.API_KEY,
+//                 "X-Auth-Date": apiHeaderTime,
+//                 "Authorization": hash4Header
+//               },
+//             }
+//           );
+//     } catch (err) {
+//         console.log(err);
+//     }
+    
+//     console.log('data');
+//     console.log(data.data.items); // check by feed?.id if exists
+// })();
